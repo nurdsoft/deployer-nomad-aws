159ff59ac58baa95cc1677e8b9d371a2a18ec2f1
Tarpan Pathak <tarpanpathak720@gmail.com>
2025-04-13 23:19:07 -0700

    Remove app-specific information

--- deploy/app/backend.tf
+++ deploy/app/backend.tf
@@ -1,7 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket = "aws-usw2-nurdsoft-terraform-state"
-    key    = "usw2/deployer-nomad/terraform.tfstate"
-    region = "us-west-2"
-  }
+  backend "s3" {}
 }
