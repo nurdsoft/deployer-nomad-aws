846757cf3d2cd3c285c2c7b3f37abcf623e9ebc1
euforia <euforia>
2025-04-23 11:49:49 -0700

    remove auth and content type check

--- main.go
+++ main.go
@@ -2,51 +2,49 @@ package main
 
 import (
 	"encoding/json"
-	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/hashicorp/nomad/api"
 
-	"deployer-nomad-aws/apikeys"
 	"deployer-nomad-aws/nomad"
 )
 
 func Entrypoint(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	authHeader := strings.TrimSpace(request.Headers["Authorization"])
-	if strings.HasPrefix(authHeader, "Bearer ") {
-		authHeader = strings.TrimSpace(strings.TrimPrefix(authHeader, "Bearer "))
-	}
+	// authHeader := strings.TrimSpace(request.Headers["Authorization"])
+	// if strings.HasPrefix(authHeader, "Bearer ") {
+	// 	authHeader = strings.TrimSpace(strings.TrimPrefix(authHeader, "Bearer "))
+	// }
 
-	if !apikeys.Have(authHeader) {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 403,
-			Body:       "Forbidden",
-		}, nil
-	}
+	// if !apikeys.Have(authHeader) {
+	// 	return events.APIGatewayProxyResponse{
+	// 		StatusCode: 403,
+	// 		Body:       "Forbidden",
+	// 	}, nil
+	// }
 
 	var (
-		reqContentType = request.Headers["Content-Type"]
-		client         = nomad.GetInstance().Jobs()
-		job            *api.Job
-		resp           events.APIGatewayProxyResponse
-		err            error
+		// reqContentType = request.Headers["Content-Type"]
+		client = nomad.GetInstance().Jobs()
+		job    *api.Job
+		resp   events.APIGatewayProxyResponse
+		err    error
 	)
 
-	switch reqContentType {
-	case "application/hcl":
-		job, err = client.ParseHCL(string(resp.Body), true)
-		if err != nil {
-			resp.StatusCode = 400
-			resp.Body = err.Error()
-			return resp, nil
-		}
-
-	default:
+	// switch reqContentType {
+	// case "application/hcl":
+	job, err = client.ParseHCL(string(resp.Body), true)
+	if err != nil {
 		resp.StatusCode = 400
-		resp.Body = "unsupported content type"
+		resp.Body = "failed to parse hcl body: " + err.Error()
 		return resp, nil
 	}
+
+	// default:
+	// 	resp.StatusCode = 400
+	// 	resp.Body = "unsupported content type"
+	// 	return resp, nil
+	// }
 
 	if validateResp, _, err := client.Validate(job, nil); err != nil {
 		b, _ := json.Marshal(validateResp)
